7b76c767d17541fedacc94327c82432ec3e89a6d
Maxime Cyr <max_maxime_max@hotmail.com>
2018-02-18 15:11:02 -0500

    Basic interface for opening a folder and seeing translation keys

--- TranslationFileEditor/Form1.Designer.cs
+++ TranslationFileEditor/Form1.Designer.cs
@@ -29,11 +29,138 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.cbMainLanguageSelector = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbKeys = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tlpTranslations = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(727, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFolderToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFolderToolStripMenuItem
+            // 
+            this.openFolderToolStripMenuItem.Name = "openFolderToolStripMenuItem";
+            this.openFolderToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openFolderToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.openFolderToolStripMenuItem.Text = "Open Folder";
+            this.openFolderToolStripMenuItem.Click += new System.EventHandler(this.openFolderToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.SelectedPath = "C:\\Users\\Maxime\\Source\\Repos\\Trendays\\Trendays.Web\\src\\assets\\languages";
+            // 
+            // cbMainLanguageSelector
+            // 
+            this.cbMainLanguageSelector.Enabled = false;
+            this.cbMainLanguageSelector.FormattingEnabled = true;
+            this.cbMainLanguageSelector.Location = new System.Drawing.Point(109, 25);
+            this.cbMainLanguageSelector.Name = "cbMainLanguageSelector";
+            this.cbMainLanguageSelector.Size = new System.Drawing.Size(121, 21);
+            this.cbMainLanguageSelector.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Main Language : ";
+            // 
+            // lbKeys
+            // 
+            this.lbKeys.Enabled = false;
+            this.lbKeys.FormattingEnabled = true;
+            this.lbKeys.Location = new System.Drawing.Point(16, 66);
+            this.lbKeys.Name = "lbKeys";
+            this.lbKeys.Size = new System.Drawing.Size(357, 472);
+            this.lbKeys.TabIndex = 3;
+            this.lbKeys.SelectedIndexChanged += new System.EventHandler(this.lbKeys_SelectedIndexChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // tlpTranslations
+            // 
+            this.tlpTranslations.AutoSize = true;
+            this.tlpTranslations.ColumnCount = 1;
+            this.tlpTranslations.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpTranslations.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpTranslations.Location = new System.Drawing.Point(380, 66);
+            this.tlpTranslations.Name = "tlpTranslations";
+            this.tlpTranslations.RowCount = 1;
+            this.tlpTranslations.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpTranslations.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpTranslations.Size = new System.Drawing.Size(335, 46);
+            this.tlpTranslations.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(727, 555);
+            this.Controls.Add(this.tlpTranslations);
+            this.Controls.Add(this.lbKeys);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbMainLanguageSelector);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Translation File Editor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFolderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ComboBox cbMainLanguageSelector;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lbKeys;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TableLayoutPanel tlpTranslations;
     }
 }
 
--- TranslationFileEditor/Form1.cs
+++ TranslationFileEditor/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,89 @@ namespace TranslationFileEditor
 {
     public partial class Form1 : Form
     {
+        private Dictionary<string, Dictionary<string, string>> TranslationsData = new Dictionary<string, Dictionary<string, string>>();
+        string MainFile = null;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void openFolderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string directory = folderBrowserDialog1.SelectedPath;
+
+                IEnumerable<string> files = Directory.EnumerateFiles(directory, "*.json").Select(x => Path.GetFileName(x));
+
+                cbMainLanguageSelector.DataSource = files.ToList();
+                cbMainLanguageSelector.Enabled = true;
+
+                foreach (string file in files)
+                {
+                    JObject obj = JObject.Parse(File.ReadAllText(directory + "/" + files.First()));
+                    Dictionary<string, string> translations = new Dictionary<string, string>();
+
+                    foreach (KeyValuePair<string, JToken> item in obj)
+                    {
+                        translations.Add(item.Key, item.Value.ToString());
+                    }
+
+                    TranslationsData.Add(file, translations);
+                }
+
+                MainFile = files.First();
+                lbKeys.DataSource = TranslationsData[MainFile].Keys.OrderBy(x => x).ToList();
+                lbKeys.Enabled = true;
+            }
+        }
+
+        private void lbKeys_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tlpTranslations.RowCount = TranslationsData.Count;
+
+            GroupBox defaultGroup = new GroupBox()
+            {
+                Text = MainFile,
+                Dock = DockStyle.Fill,
+                MaximumSize = new Size(0, 100)
+            };
+
+            defaultGroup.Controls.Add(new TextBox()
+            {
+                Text = TranslationsData[MainFile][lbKeys.SelectedValue.ToString()],
+                Top = 20,
+                Left = 20,
+                Width = 200
+            });
+            
+            tlpTranslations.Controls.Add(defaultGroup, 0, 0);
+
+            foreach(string file in TranslationsData.Keys.Where(x => x != MainFile))
+            {
+                GroupBox group = new GroupBox()
+                {
+                    Text = file,
+                    Dock = DockStyle.Fill,
+                    MaximumSize = new Size(0, 100)
+                };
+
+                group.Controls.Add(new TextBox()
+                {
+                    Text = TranslationsData[file][lbKeys.SelectedValue.ToString()],
+                    Top = 20,
+                    Left = 20,
+                    Width = 200
+                });
+
+                tlpTranslations.Controls.Add(group, 0, 1);
+            }
         }
     }
 }
